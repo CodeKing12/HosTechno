afdbc3b97478418ad801fabf94d86ebf5d32b27c
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-02-01 02:03:48 +0100

    I'm done with the pricing page.

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
+
+let all_prices = document.getElementsByClassName("aplan");
+function priceColoring(hovered) {
+    for (let a=0; a < all_prices.length; a++) {
+        all_prices[a].classList.remove("highlight-plan")
+        hovered.classList.add("highlight-plan")
+    }
+}
+
+function backToDefault() {
+    let priceDefault = document.getElementById("highPlan")
+    for (let a=0; a < all_prices.length; a++) {
+        all_prices[a].classList.remove("highlight-plan")
+    }
+    priceDefault.classList.add("highlight-plan")
+}
